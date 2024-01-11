19c85ae4a9e39c5791e044c1a722589f203b7c21
B e n g i <bambang.trd17@gmail.com>
2024-01-11 14:20:31 +0700

    feat: use catppuccin colorscheme inside AstroVim

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,45 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  
+  -- Catppuccin for (Neo)bvim  
+  { 
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "mocha",
+        dark = "mocha",
+      },
+      integrations = {
+        alpha = true,
+        aerial = true,
+        dap = { enabled = true, enable_ui = true },
+        mason = true,
+        neotree = true,
+        notify = true,
+        nvimtree = false,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        ts_rainbow = false,
+        which_key = true,
+      },
+    },
+  },
+
+  -- Telescope
+   {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    },
+  },
+  
 }
+
